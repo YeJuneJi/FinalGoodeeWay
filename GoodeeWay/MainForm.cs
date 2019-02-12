bc4a101a4db1c9293eca5ff16d769d6ca586a7fe
godn0117 <godn0117@gmail.com>
2019-02-12 16:18:33 +0900

    ㅇㅇ

--- GoodeeWay/MainForm.cs
+++ GoodeeWay/MainForm.cs
@@ -19,38 +19,14 @@ namespace GoodeeWay
             InitializeComponent();
         }
 
-        
         private void MainForm_Load(object sender, EventArgs e)
         {
            
         }
-
-        bool on_off = false;
-        float time = 0f;
-
         private void button1_Click(object sender, EventArgs e)
         {
             OderVIew oView = new OderVIew();
             oView.ShowDialog();
-
-            //if (on_off == true)
-            //{
-            //    timer1.Stop();
-            //    on_off = false;
-            //    textBox1.Text = time.ToString() + "초";
-            //}
-            //else if (on_off == false)
-            //{
-            //    time = 0f;
-            //    timer1.Start();
-            //    on_off = true;
-            //}
-
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            time += 0.1f;
         }
     }
 }
