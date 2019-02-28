a9345e8435049f02d091336e27c8ff55cc268890
kovv07 <kovv07@gmail.com>
2019-02-28 10:57:55 +0900

    컨트롤패널로 옮기고 빠진 이벤트 추가

--- GoodeeWay/BUS/EmployeeControl.Designer.cs
+++ GoodeeWay/BUS/EmployeeControl.Designer.cs
@@ -281,6 +281,7 @@
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(1200, 357);
             this.dataGridView1.TabIndex = 116;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // btnSearch
             // 
